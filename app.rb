ab5dfe4b23c105ea593091ad34e6fa5f71c600d6
Faezrah Rizalman <fwrizalman@gmail.com>
2014-06-28 22:41:16 +0100

    Added route and erb files for adding new posts

--- app.rb
+++ app.rb
@@ -12,6 +12,21 @@ get '/' do
   erb :'posts/index'
 end
 
+get '/posts/create' do
+ @title = 'Create post'
+ @post = Post.new
+ erb :'posts/create'
+end
+
+post "/posts" do
+ @post = Post.new(params[:post])
+ if @post.save
+   redirect "posts/#{@post.id}"
+ else
+   erb :"posts/create"
+ end
+end
+
 get '/posts/:id' do
  @post = Post.find(params[:id])
  @title = @post.title
