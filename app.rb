c53e404d534ea9bcb681a826ab4c5e4ab03ab856
Faezrah Rizalman <fwrizalman@gmail.com>
2014-06-28 22:47:09 +0100

    Added flash notice

--- app.rb
+++ app.rb
@@ -1,7 +1,11 @@
 require 'sinatra'
 require 'sinatra/activerecord'
+require 'sinatra/flash'
+require 'sinatra/redirect_with_flash'
+
 require './environments'
 
+enable :sessions
 
 class Post < ActiveRecord::Base
   validates :title, presence: true, length: { minimum: 5 }
@@ -15,24 +19,24 @@ get '/' do
 end
 
 get '/posts/create' do
- @title = 'Create post'
- @post = Post.new
- erb :'posts/create'
+  @title = 'Create post'
+  @post = Post.new
+  erb :'posts/create'
 end
 
-post "/posts" do
- @post = Post.new(params[:post])
- if @post.save
-   redirect "posts/#{@post.id}"
- else
-   erb :"posts/create"
- end
+post '/posts' do
+  @post = Post.new(params[:post])
+  if @post.save
+    redirect 'posts/#{@post.id}', :notice => 'Congrats! Love the new post. (This message will disapear in 4 seconds.)'
+  else
+    redirect 'posts/create', :error => 'Something went wrong. Try again. (This message will disapear in 4 seconds.)'
+  end
 end
 
 get '/posts/:id' do
- @post = Post.find(params[:id])
- @title = @post.title
- erb :'posts/view'
+  @post = Post.find(params[:id])
+  @title = @post.title
+  erb :'posts/view'
 end
 
 helpers do
