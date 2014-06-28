106c7f0c9b747ba0c2ea71673a78c06c6fdb2680
Faezrah Rizalman <fwrizalman@gmail.com>
2014-06-28 22:13:31 +0100

    Db folder created automatically

--- db/migrate/20140628210619_create_posts.rb
+++ db/migrate/20140628210619_create_posts.rb
@@ -1,4 +1,13 @@
 class CreatePosts < ActiveRecord::Migration
-  def change
+  def self.up
+    create_table :posts do |t|
+      t.string :title
+      t.text :body
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :posts
   end
 end
