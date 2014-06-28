acd91eb2338d2cd9d5f256962567132409250c3e
Faezrah Rizalman <fwrizalman@gmail.com>
2014-06-28 22:30:18 +0100

    Added get route and a little helper method

--- app.rb
+++ app.rb
@@ -5,3 +5,19 @@ require './environments'
 
 class Post < ActiveRecord::Base
 end
+
+get '/' do
+  @posts = Post.order('created_at DESC')
+  @title = 'Welcome.'
+  erb :'posts/index'
+end
+
+helpers do
+  def title
+    if @title
+      '#{@title}'
+    else
+      'Welcome.'
+    end
+  end
+end
